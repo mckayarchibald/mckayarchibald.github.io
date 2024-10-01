8c41f391234c941bba78bc1ae7202f9aacab2537
McKay Archibald <mck.archibald@gmail.com>
2024-10-01 13:32:27 -0600

    update personal landing page

--- archive/v4/tailwind.config.ts
+++ archive/v4/tailwind.config.ts
@@ -3,7 +3,9 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ["League+Spartan"],
+    },
   },
   plugins: [],
 } satisfies Config;
